5e825ee495ce28d83936953edeaff626f4ec5237
MD Ibna Abid <mdabout2@gmail.com>
2026-07-10 12:04:15 +0600

    import all file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+
 import BambooHero from "./Components/Hero";
+import BestSellers from "./Components/BestSeller";
+import FeaturedProducts from "./Components/Product.";
+import WhyChooseUs from "./Components/WhyChoose";
+import BlogSection from "./Components/Blog";
+import FAQSection from "./Components/Faq";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
    <>
    <BambooHero/>
+   <FeaturedProducts/>
+   <BestSellers/>
+   <WhyChooseUs/>
+   <BlogSection/>
+   <FAQSection/>
+   <Footer/>
    </>
   );
 }
